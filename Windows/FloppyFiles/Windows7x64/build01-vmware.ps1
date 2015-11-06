247d237514c0a6c24c043f42e3c7547b370ac309
Wil Taylor <Wil Taylor>
2015-11-07 00:35:25 +1100

    First working version

--- Windows/FloppyFiles/Windows7x64/build01-vmware.ps1
+++ Windows/FloppyFiles/Windows7x64/build01-vmware.ps1
@@ -5,16 +5,15 @@ if(-not(Test-Path 'c:\windows\temp\tools-windows.tar'))
     $client.DownloadFile('http://softwareupdate.vmware.com/cds/vmw-desktop/ws/12.0.1/3160714/windows/packages/tools-windows.tar','c:\windows\temp\tools-windows.tar')
 }
 
-if(-not(Test-Path 'c:\windows\temp\7zip.zip'))
+if(-not(Test-Path 'c:\windows\temp\7z920-x64.msi'))
 {
-  $client.DownloadFile('http://www.7-zip.org/a/7za920.zip', 'c:\windows\temp\7zip.zip')
+  $client.DownloadFile('http://www.7-zip.org/a/7z920-x64.msi', 'c:\windows\temp\7z920-x64.msi')
 }
 
-$shell_app=new-object -com shell.application
-$zip = $shell_app.namespace("c:\windows\temp\7zip.zip")
-$dest = $shell_app.namespace("c:\windows\temp")
-$dest.copyhere($zip.items())
+&"c:\windows\system32\msiexec.exe" /i 'c:\windows\temp\7z920-x64.msi' /qn REBOOT=ReallySupress | out-null
 
-&"c:\windows\temp\7za.exe" x "c:\windows\temp\tools-windows.tar" -oC:\windows\temp | out-null
-&"c:\windows\temp\7za.exe" x "c:\windows\temp\VMware-tools-windows-10.0.1-3160059.iso" -oC:\windows\temp | out-null
-&"c:\windows\temp\setup.exe /s /v`"/qn REBOOT=ReallySupress`"" | out-null
+&"C:\Program Files\7-Zip\7z.exe" x -y "c:\windows\temp\tools-windows.tar" -oC:\windows\temp | out-null
+&"C:\Program Files\7-Zip\7z.exe" x -y "c:\windows\temp\VMware-tools-windows-10.0.1-3160059.iso" -oC:\windows\temp | out-null
+&"c:\windows\temp\setup.exe" /s /v/qn | out-null
+
+&"c:\windows\system32\msiexec.exe" /x 'c:\windows\temp\7z920-x64.msi' /qn | out-null
