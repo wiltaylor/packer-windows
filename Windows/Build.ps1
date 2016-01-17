507bf37afc5e41b68ef917323cb7a809a9a9b23c
Wil Taylor <wilfridtaylor@gmail.com>
2016-01-17 22:54:22 +1100

    Changed from full build to from template build

---
+++ Windows/Build.ps1
@@ -0,0 +1,12 @@
+﻿Set-Location 'F:\Virtual Machines\BaseOS\Windows'
+
+foreach($item in Get-ChildItem 'F:\Virtual Machines\BaseOS\Windows' -Filter *.json) {
+    &packer build $item.Name
+}
+
+
+foreach($item in Get-ChildItem 'F:\Virtual Machines\BaseOS\Windows' -Filter *.box) {
+    $name = $item.Name.Replace(".box", "")
+
+    &vagrant box add -name $name --force $item.FullName
+}
