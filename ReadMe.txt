16db0d573c5d698777830461f05701519eb8c119
Wil Taylor <wilfridtaylor@gmail.com>
2016-02-01 21:53:13 +1100

    Windows 7 vm building boxes now

---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+To get this package working you need to do the following:
+
+* Copy iso files into the windows\iso folder.
+* Copy upgrader.exe to windows\tools. This file is insde the floppy image created by vmware when using quick install feature.
--- Windows/scripts/vagrantuser.cmd
+++ Windows/scripts/vagrantuser.cmd
@@ -5,4 +5,7 @@ wmic useraccount where "name='vagrant'" set PasswordExpires=FALSE
 
 net accounts /MAXPWAGE:UNLIMITED
 
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d 0 /f
+
+copy a:\sysprep.xml c:\windows\temp\sysprep.xml
