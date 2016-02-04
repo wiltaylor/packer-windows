8d97a83a2e43a5ed1f223f6c58ff73b0f9333562
Wil Taylor <wilfridtaylor@gmail.com>
2016-02-04 17:43:31 +1100

    Cleaned out legacy scripts that are not used any more.

--- Windows/scripts/disableautoupdate.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-reg.exe add "HKLM\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1
-reg.exe add "HKLM\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUState /t REG_DWORD /d 7
--- Windows/scripts/powershell.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-Echo Setting powershell execution policy
-C:\windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
-C:\windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
